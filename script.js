de9ece672b7e07717eda818820b4fa459a28fa97
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-03-31 16:37:13 +1100

    Finalised the positioning and look of subtask. Began the initial stages of subtask's functionality. Commented out displaying subtask until it is completed so that the current usability is not affected

--- script.js
+++ script.js
@@ -16,11 +16,32 @@ function addTask(){
         let li = document.createElement("li"); // create an 'li', store it in variable li
         li.innerHTML = inputBox.value; // give the li the text in the inputBox
         listContainer.appendChild(li); // append the new li to the listContainer (the ul)
-        
+
+        // Creating subtaskContainer (div) and subtask (button)
+        let subtaskContainer = document.createElement("div");
+        subtaskContainer.setAttribute("id", "subtaskContainer")
+        let subtask = document.createElement("button");
+        subtask.setAttribute("id", "subtask");
+        subtask.textContent = "Add subtask"; //setting textContent
+        // Appending subtask to the container (put button in div)
+        subtaskContainer.appendChild(subtask);
+
+        //Appending subtask to the listContainer 
+        // COMMENTED OUT DISPLAYING SUBTASK FOR CLEANLINESS OF CURRENT PROJECT
+        //li.appendChild(subtaskContainer); //appending the container to the list
+
         // Creating the x after the task
         let span = document.createElement("span"); // create new element
         span.innerHTML = "\u00d7"; // set it's html to 'x' (unicode for the character "x")
         li.appendChild(span); // append span to the li
+
+        //On clicking subtask
+        subtask.addEventListener("click", function(e){
+            let subtaskInput = document.createElement("input");
+            subtaskContainer.appendChild(subtaskInput);
+        
+        });
+        
 
         saveTasks(); // Save the added tasks to local storage
     }
@@ -38,6 +59,7 @@ listContainer.addEventListener("click", function(e){
     }
     
 }, false);
+
 
 function saveTasks(){
     localStorage.setItem("tasks", listContainer.innerHTML); // saves the listContainer's HTML
